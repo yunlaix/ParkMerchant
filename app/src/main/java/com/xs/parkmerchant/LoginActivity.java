b996bfc8eac198d3cb38c399a1594c5077098a79
yunlaix <2909981640@qq.com>
2016-07-07 12:47:35 +0800

    test

--- app/src/main/java/com/xs/parkmerchant/LoginActivity.java
+++ app/src/main/java/com/xs/parkmerchant/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.xs.parkmerchant;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,15 +12,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.xs.parkmerchant.Net.Constants;
 import com.xs.parkmerchant.Net.NetCore;
 import com.xs.parkmerchant.Net.Url;
-
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,26 +26,35 @@ import java.util.List;
  */
 public class LoginActivity extends AppCompatActivity{
 
-    private EditText editText_name, editText_password;
-    private TextView textView_login;
+    private EditText editText_id, editText_password;
+    private TextView textView_login, textView_register;
+    private SharedPreferences sharedPreferences;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what==1){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //init
-        editText_name = (EditText) findViewById(R.id.seller_name);
+        editText_id = (EditText) findViewById(R.id.seller_id);
         editText_password = (EditText) findViewById(R.id.seller_password);
         textView_login = (TextView) findViewById(R.id.login);
-
-        //auto-login
-
+        textView_register = (TextView) findViewById(R.id.register);
+        sharedPreferences = getSharedPreferences("login_parkmerchant", MODE_PRIVATE);
         textView_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //login
                 getEditText();
-                if(Constants.seller_name=="" || Constants.seller_password==""){
+                if(Constants.seller_id=="" || Constants.seller_password==""){
                     Toast.makeText(getApplicationContext(), "empty!", Toast.LENGTH_SHORT).show();
                 }else{
                     new Thread(new Runnable() {
@@ -52,7 +62,7 @@ public class LoginActivity extends AppCompatActivity{
                         public void run() {
                             try {
                                 List<NameValuePair> params = new ArrayList<NameValuePair>();
-                                params.add(new BasicNameValuePair("seller_id", Constants.seller_name));
+                                params.add(new BasicNameValuePair("seller_id", Constants.seller_id));
                                 params.add(new BasicNameValuePair("seller_password", Constants.seller_password));
                                 String result = NetCore.postResulttoNet(Url.login_1, params);
                                 Log.d("login", "result"+result);
@@ -60,6 +70,8 @@ public class LoginActivity extends AppCompatActivity{
                                     JSONObject jsonObject = new JSONObject(result);
                                     String state = jsonObject.getString("state");
                                     if(state.equals("0")){
+                                        setSharePreference();
+                                        handler.sendEmptyMessage(1);
                                         Log.d("login", "ssssssssssssssssss");
                                     }else{
                                         Log.d("login", "fffffffffffffffff");
@@ -73,11 +85,45 @@ public class LoginActivity extends AppCompatActivity{
                 }
             }
         });
+
+        textView_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void getEditText(){
-        Constants.seller_name = editText_name.getText().toString().trim();
+        Constants.seller_id = editText_id.getText().toString().trim();
         Constants.seller_password = editText_password.getText().toString().trim();
+    }
+
+    private void setSharePreference(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("seller_id", Constants.seller_id);
+        editor.putString("seller_password", Constants.seller_password);
+        //other info
+        editor.commit();
+        Log.d("login", "A"+sharedPreferences.getString("seller_id", "")+sharedPreferences.getString("seller_password", "")+"B");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //auto-login
+        Constants.seller_id = sharedPreferences.getString("seller_id", "");
+        Constants.seller_password = sharedPreferences.getString("seller_password", "");
+        if(!Constants.seller_id.equals("") && !Constants.seller_password.equals("")){
+            Constants.seller_name = sharedPreferences.getString("seller_name", "");
+            Constants.seller_address = sharedPreferences.getString("seller_address", "");
+            Constants.seller_contact = sharedPreferences.getString("seller_contact", "");
+            Constants.seller_img = sharedPreferences.getString("seller_img", "");
+            Log.d("login", "auto-login"+Constants.seller_id+Constants.seller_password);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
 }
